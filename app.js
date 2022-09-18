209dbe8c116fc407534136785588e3227991ca64
Swank <willmadedesign@gmail.com>
2022-09-18 15:07:07 -0700

    Create math operator functions

---
+++ app.js
@@ -0,0 +1,20 @@
+// Start by creating operator functions
+function add(num1, num2) {
+   let sum = num1 + num2;
+   return sum;
+} 
+
+ function subtract(num1, num2) {
+    let dif = num1 - num2;
+    return dif;
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+function divide (num1, num2) {
+    let quot = num1 / num2;
+    return quot;
+}
