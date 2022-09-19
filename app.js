b978e1f9e9ed9ef4d2d0ec43e62e27028fdb40bd
Swank <willmadedesign@gmail.com>
2022-09-19 16:08:04 -0700

    Construct calc class to store data

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+// Calculator class that stores all the data when buttons are selected
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        //where the text will be appended
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear();
+    }
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNum(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+}
+
+
+
 // Button selectors
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
